92956e7dfa9a18f7eaa4d6cd62629ae11343b477
Adrian Wium <adrian.wium@outlook.com>
2023-07-28 14:25:39 +0200

    Changed env to "local" and ignore case when parsing

--- src/api/src/domain/Yoma.Core.Domain/Core/Helpers/EnvironmentHelper.cs
+++ src/api/src/domain/Yoma.Core.Domain/Core/Helpers/EnvironmentHelper.cs
@@ -9,7 +9,7 @@
                 throw new ArgumentNullException(nameof(environment));
 
             var ret = Environment.None;
-            if (!Enum.TryParse(environment, out ret) || ret == Environment.None)
+            if (!Enum.TryParse(environment, true, out ret) || ret == Environment.None)
                 throw new ArgumentException($"Environment of '{environment}' not supported", nameof(environment));
 
             return ret;
