c136debbf2abdfaba9fca77337da30a626733336
Adrian Wium <adrian.wium@outlook.com>
2023-08-01 15:42:16 +0200

    Added custom ReformatValidationProblemAttribute

---
+++ src/api/src/application/Yoma.Core.Api/Middleware/ReformatValidationProblemAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.ComponentModel.DataAnnotations;
+using Yoma.Core.Domain.Core.Models;
+
+namespace Yoma.Core.Api.Middleware
+{
+    public class ReformatValidationProblemAttribute : ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            if (context.Result is BadRequestObjectResult badRequestObjectResult)
+            {
+                var validationProblemDetails = badRequestObjectResult.Value as ValidationProblemDetails;
+                if (validationProblemDetails != null && validationProblemDetails.Errors.Any())
+                {
+                    var errorResponse = new List<ErrorResponseItem>();
+                    foreach (var error in validationProblemDetails.Errors.Values)
+                    {
+                        errorResponse.Add(
+                            new ErrorResponseItem
+                            {
+                                Type = nameof(ValidationException),
+                                Message = string.Join(", ", error.Select(o => o))
+                            });
+                    }
+                    context.Result = new BadRequestObjectResult(errorResponse);
+                }
+            }
+
+            base.OnResultExecuting(context);
+        }
+    }
+}
