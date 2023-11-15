96b7da72b3ca1ec07589341f1fbdef00da826998
wdbasson <wlmbasson@gmail.com>
2023-11-15 15:40:42 +0200

    293 initial stage environment assets (#297)
    * Initial programmatic keycloak users
    
    * LB subnets
    
    * postgresql-keycloak secrets
    
    * yoma-api staging initial commit
    
    * yoma-web staging initial commit
    
    * post install hook for keycloak
    
    * dev conf fixes
    
    * automate post.sql
    
    * automated KC user creation & DB init
    
    * remove debug code
    
    * Change to master branch gh ref

--- src/api/cicd/scripts/keycloak/users.sh
+++ src/api/cicd/scripts/keycloak/users.sh
@@ -6,7 +6,7 @@
 # KC_CLIENT_ID="admin-cli"
 # KC_ADMIN_USER="xxxxx@example.com"
 # KC_ADMIN_PASSWORD="xxxxx"
-KC_JWT=$(curl -s -X POST "${KC_BASE_URL}/realms/${KC_ADMIN_REALM}/protocol/openid-connect/token" \
+KC_JWT=$(curl -s -X POST "${KC_BASE_URL}/realms/${KC_ADMIN_REALM:-master}/protocol/openid-connect/token" \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -d 'grant_type=password' \
   -d "client_id=${KC_CLIENT_ID}" \
@@ -25,130 +25,140 @@ roleIDs=$(curl -s -X GET "${KC_BASE_URL}/admin/realms/${KC_REALM}/roles" \
   }')
 
 # Test Admin User
-echo "Creating Test Admin User"
-curl -s -X POST "${KC_BASE_URL}/admin/realms/${KC_REALM}/users" \
-  -H 'Content-Type: application/json' \
-  -H "Authorization: bearer ${KC_JWT}" \
-  -d '{
-        "username": "testadminuser@gmail.com",
-        "enabled": true,
-        "emailVerified": true,
-        "firstName": "Test Admin",
-        "lastName": "User",
-        "email": "testadminuser@gmail.com",
-        "attributes": {
-          "dateOfBirth": ["01/01/2001"],
-          "phoneNumber": ["202-918-2132"],
-          "countryOfOrigin": ["Afghanistan"],
-          "countryOfResidence": ["Afghanistan"],
-          "gender": ["Male"]
-        },
-        "credentials": [{
-          "type": "password",
-          "value": "P@ssword1",
-          "temporary": false
-        }],
-        "realmRoles": ["default-roles-yoma", "Admin", "User"]
-      }'
-# Get Test Admin User ID
-adminUserID=$(curl -s -X GET "${KC_BASE_URL}/admin/realms/${KC_REALM}/users?exact=true&username=testadminuser@gmail.com" \
-  -H 'Content-Type: application/json' \
-  -H "Authorization: bearer ${KC_JWT}" | jq .[].id -r)
-# Assign Test Admin User to required roles
-curl -s -X POST "${KC_BASE_URL}/admin/realms/${KC_REALM}/users/${adminUserID}/role-mappings/realm" \
-  -H 'Content-Type: application/json' \
-  -H "Authorization: bearer ${KC_JWT}" \
-  -d "[
-        {
-          \"id\": \"$(echo ${roleIDs} | jq .admin -r)\",
-          \"name\": \"Admin\"
-        },
-        {
-          \"id\": \"$(echo ${roleIDs} | jq .user -r)\",
-          \"name\": \"User\"
-        }
-      ]"
+if [ ! -z "${ADMIN_USER}" ]; then
+  echo "Creating Test Admin User: ${ADMIN_USER}"
+  curl -s -X POST "${KC_BASE_URL}/admin/realms/${KC_REALM}/users" \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: bearer ${KC_JWT}" \
+    -d '{
+          "username": "'"${ADMIN_USER}"'",
+          "enabled": true,
+          "emailVerified": true,
+          "firstName": "Test Admin",
+          "lastName": "User",
+          "email": "'"${ADMIN_USER}"'",
+          "attributes": {
+            "dateOfBirth": ["01/01/2001"],
+            "phoneNumber": ["202-918-2132"],
+            "countryOfOrigin": ["Afghanistan"],
+            "countryOfResidence": ["Afghanistan"],
+            "gender": ["Male"]
+          },
+          "credentials": [{
+            "type": "password",
+            "value": "'"${ADMIN_USER_PASSWORD}"'",
+            "temporary": false
+          }],
+          "realmRoles": ["default-roles-yoma", "Admin", "User"]
+        }'
+  # Get Test Admin User ID
+  echo "Geting Test Admin User: ${ADMIN_USER}"
+  adminUserID=$(curl -s -X GET "${KC_BASE_URL}/admin/realms/${KC_REALM}/users?exact=true&username=${ADMIN_USER}" \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: bearer ${KC_JWT}" | jq .[].id -r)
+  # Assign Test Admin User to required roles
+  echo "Assigning Test Admin User: ${ADMIN_USER}"
+  curl -s -X POST "${KC_BASE_URL}/admin/realms/${KC_REALM}/users/${adminUserID}/role-mappings/realm" \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: bearer ${KC_JWT}" \
+    -d "[
+          {
+            \"id\": \"$(echo ${roleIDs} | jq .admin -r)\",
+            \"name\": \"Admin\"
+          },
+          {
+            \"id\": \"$(echo ${roleIDs} | jq .user -r)\",
+            \"name\": \"User\"
+          }
+        ]"
+fi
+
 # Test Org Admin User
-echo "Creating Test Org Admin User"
-curl -s -X POST "${KC_BASE_URL}/admin/realms/${KC_REALM}/users" \
-  -H 'Content-Type: application/json' \
-  -H "Authorization: bearer ${KC_JWT}" \
-  -d '{
-        "username": "testorgadminuser@gmail.com",
-        "enabled": true,
-        "emailVerified": true,
-        "firstName": "Test Organization Admin",
-        "lastName": "User",
-        "email": "testorgadminuser@gmail.com",
-        "attributes": {
-          "dateOfBirth": ["01/01/2001"],
-          "phoneNumber": ["202-918-2132"],
-          "countryOfOrigin": ["Afghanistan"],
-          "countryOfResidence": ["Afghanistan"],
-          "gender": ["Male"]
-        },
-        "credentials": [{
-          "type": "password",
-          "value": "P@ssword1",
-          "temporary": false
-        }],
-        "realmRoles": ["default-roles-yoma", "OrganisationAdmin", "User"]
-      }'
-# Get Test Organisation Admin User ID
-orgAdminUserID=$(curl -s -X GET "${KC_BASE_URL}/admin/realms/${KC_REALM}/users?exact=true&username=testorgadminuser@gmail.com" \
-  -H 'Content-Type: application/json' \
-  -H "Authorization: bearer ${KC_JWT}" | jq .[].id -r)
-# Assign Test Organisation Admin User to required roles
-curl -s -X POST "${KC_BASE_URL}/admin/realms/${KC_REALM}/users/${orgAdminUserID}/role-mappings/realm" \
-  -H 'Content-Type: application/json' \
-  -H "Authorization: bearer ${KC_JWT}" \
-  -d "[
-        {
-          \"id\": \"$(echo ${roleIDs} | jq .orgAdmin -r)\",
-          \"name\": \"OrganisationAdmin\"
-        },
-        {
-          \"id\": \"$(echo ${roleIDs} | jq .user -r)\",
-          \"name\": \"User\"
-        }
-      ]"
+if [ ! -z "${ORG_ADMIN_USER}" ]; then
+  echo "Creating Test Org Admin User"
+  curl -s -X POST "${KC_BASE_URL}/admin/realms/${KC_REALM}/users" \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: bearer ${KC_JWT}" \
+    -d '{
+          "username": "'"${ORG_ADMIN_USER}"'",
+          "enabled": true,
+          "emailVerified": true,
+          "firstName": "Test Organization Admin",
+          "lastName": "User",
+          "email": "'"${ORG_ADMIN_USER}"'",
+          "attributes": {
+            "dateOfBirth": ["01/01/2001"],
+            "phoneNumber": ["202-918-2132"],
+            "countryOfOrigin": ["Afghanistan"],
+            "countryOfResidence": ["Afghanistan"],
+            "gender": ["Male"]
+          },
+          "credentials": [{
+            "type": "password",
+            "value": "'"${ORG_ADMIN_USER_PASSWORD}"'",
+            "temporary": false
+          }],
+          "realmRoles": ["default-roles-yoma", "OrganisationAdmin", "User"]
+        }'
+  # Get Test Organisation Admin User ID
+  orgAdminUserID=$(curl -s -X GET "${KC_BASE_URL}/admin/realms/${KC_REALM}/users?exact=true&username=${ORG_ADMIN_USER}" \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: bearer ${KC_JWT}" | jq .[].id -r)
+  # Assign Test Organisation Admin User to required roles
+  curl -s -X POST "${KC_BASE_URL}/admin/realms/${KC_REALM}/users/${orgAdminUserID}/role-mappings/realm" \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: bearer ${KC_JWT}" \
+    -d "[
+          {
+            \"id\": \"$(echo ${roleIDs} | jq .orgAdmin -r)\",
+            \"name\": \"OrganisationAdmin\"
+          },
+          {
+            \"id\": \"$(echo ${roleIDs} | jq .user -r)\",
+            \"name\": \"User\"
+          }
+        ]"
+fi
+
 # Test User
-echo "Creating Test User"
-curl -s -X POST "${KC_BASE_URL}/admin/realms/${KC_REALM}/users" \
-  -H 'Content-Type: application/json' \
-  -H "Authorization: bearer ${KC_JWT}" \
-  -d '{
-        "username": "testuser@gmail.com",
-        "enabled": true,
-        "emailVerified": true,
-        "firstName": "Test",
-        "lastName": "User",
-        "email": "testuser@gmail.com",
-        "attributes": {
-          "dateOfBirth": ["01/01/2001"],
-          "phoneNumber": ["202-918-2132"],
-          "countryOfOrigin": ["Afghanistan"],
-          "countryOfResidence": ["Afghanistan"],
-          "gender": ["Male"]
-        },
-        "credentials": [{
-          "type": "password",
-          "value": "P@ssword1",
-          "temporary": false
-        }],
-        "realmRoles": ["default-roles-yoma", "User"]
-      }'
-# Get Test User ID
-userID=$(curl -s -X GET "${KC_BASE_URL}/admin/realms/${KC_REALM}/users?exact=true&username=testuser@gmail.com" \
-  -H 'Content-Type: application/json' \
-  -H "Authorization: bearer ${KC_JWT}" | jq .[].id -r)
-# Assign Test User to required roles
-curl -s -X POST "${KC_BASE_URL}/admin/realms/${KC_REALM}/users/${userID}/role-mappings/realm" \
-  -H 'Content-Type: application/json' \
-  -H "Authorization: bearer ${KC_JWT}" \
-  -d "[
-        {
-          \"id\": \"$(echo ${roleIDs} | jq .user -r)\",
-          \"name\": \"User\"
-        }
-      ]"
+if [ ! -z "${TEST_USER}" ]; then
+  echo "Creating Test User"
+  curl -s -X POST "${KC_BASE_URL}/admin/realms/${KC_REALM}/users" \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: bearer ${KC_JWT}" \
+    -d '{
+          "username": "'"${TEST_USER}"'",
+          "enabled": true,
+          "emailVerified": true,
+          "firstName": "Test",
+          "lastName": "User",
+          "email": "'"${TEST_USER}"'",
+          "attributes": {
+            "dateOfBirth": ["01/01/2001"],
+            "phoneNumber": ["202-918-2132"],
+            "countryOfOrigin": ["Afghanistan"],
+            "countryOfResidence": ["Afghanistan"],
+            "gender": ["Male"]
+          },
+          "credentials": [{
+            "type": "password",
+            "value": "'"${TEST_USER_PASSWORD}"'",
+            "temporary": false
+          }],
+          "realmRoles": ["default-roles-yoma", "User"]
+        }'
+  # Get Test User ID
+  userID=$(curl -s -X GET "${KC_BASE_URL}/admin/realms/${KC_REALM}/users?exact=true&username=${TEST_USER}" \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: bearer ${KC_JWT}" | jq .[].id -r)
+  # Assign Test User to required roles
+  curl -s -X POST "${KC_BASE_URL}/admin/realms/${KC_REALM}/users/${userID}/role-mappings/realm" \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: bearer ${KC_JWT}" \
+    -d "[
+          {
+            \"id\": \"$(echo ${roleIDs} | jq .user -r)\",
+            \"name\": \"User\"
+          }
+        ]"
+fi
---
+++ src/api/cicd/scripts/sqlserver-init/post-stage.sql
@@ -0,0 +1,170 @@
+USE [yoma-stage]
+GO
+/*****user & organization*****/
+INSERT INTO [Entity].[User]([Id],[Email],[EmailConfirmed],[FirstName],[Surname],[DisplayName],[PhoneNumber],[CountryId],[CountryOfResidenceId],
+			[PhotoId],[GenderId],[DateOfBirth],[DateLastLogin],[ExternalId],[ZltoWalletId],[DateZltoWalletCreated],[YoIDOnboarded],[DateYoIDOnboarded],[DateCreated],[DateModified])
+VALUES(NEWID(),'{test_user}',1,'Sam','Henderson','Sam Henderson',null,(SELECT [Id] FROM [Lookup].[Country] WHERE CodeAlpha2 = 'ZA'),(SELECT [Id] FROM [Lookup].[Country] WHERE CodeAlpha2 = 'ZA'),
+		NULL,(SELECT TOP 1 [Id] FROM [Lookup].[Gender] WHERE [Name] = 'Male'),NULL,NULL,NULL,NULL,NULL,1,GETDATE(),GETDATE(),GETDATE())
+GO
+
+INSERT INTO [Entity].[User]([Id],[Email],[EmailConfirmed],[FirstName],[Surname],[DisplayName],[PhoneNumber],[CountryId],[CountryOfResidenceId],
+			[PhotoId],[GenderId],[DateOfBirth],[DateLastLogin],[ExternalId],[ZltoWalletId],[DateZltoWalletCreated],[YoIDOnboarded],[DateYoIDOnboarded],[DateCreated],[DateModified])
+VALUES(NEWID(),'{org_admin_user}',1,'Sam','Henderson','Sam Henderson',null,(SELECT [Id] FROM [Lookup].[Country] WHERE CodeAlpha2 = 'ZA'),(SELECT [Id] FROM [Lookup].[Country] WHERE CodeAlpha2 = 'ZA'),
+		NULL,(SELECT TOP 1 [Id] FROM [Lookup].[Gender] WHERE [Name] = 'Male'),NULL,NULL,NULL,NULL,NULL,1,GETDATE(),GETDATE(),GETDATE())
+GO
+
+INSERT INTO [Entity].[User]([Id],[Email],[EmailConfirmed],[FirstName],[Surname],[DisplayName],[PhoneNumber],[CountryId],[CountryOfResidenceId],
+			[PhotoId],[GenderId],[DateOfBirth],[DateLastLogin],[ExternalId],[ZltoWalletId],[DateZltoWalletCreated],[YoIDOnboarded],[DateYoIDOnboarded],[DateCreated],[DateModified])
+VALUES(NEWID(),'{admin_user}',1,'Sam','Henderson','Sam Henderson',null,(SELECT [Id] FROM [Lookup].[Country] WHERE CodeAlpha2 = 'ZA'),(SELECT [Id] FROM [Lookup].[Country] WHERE CodeAlpha2 = 'ZA'),
+		NULL,(SELECT TOP 1 [Id] FROM [Lookup].[Gender] WHERE [Name] = 'Male'),NULL,NULL,NULL,NULL,NULL,1,GETDATE(),GETDATE(),GETDATE())
+GO
+
+--ssi credential issuance (pending) for YOID onboarded users
+INSERT INTO [SSI].[CredentialIssuance]([Id],[SchemaTypeId],[ArtifactType],[SchemaName],[SchemaVersion],[StatusId],[UserId],[OrganizationId]
+           ,[MyOpportunityId],[CredentialId],[ErrorReason],[RetryCount],[DateCreated],[DateModified])
+SELECT NEWID(),(SELECT [Id] FROM [SSI].[SchemaType] WHERE [Name] = 'YoID'),'Indy','YoID|Default','1.0',(SELECT [Id] FROM [SSI].[CredentialIssuanceStatus] WHERE [Name] = 'Pending'),
+	U.[Id],NULL,NULL,NULL,NULL,NULL,GETDATE(),GETDATE()
+FROM [Entity].[User] U
+WHERE U.[YoIDOnboarded] = 1
+GO
+
+--Yoma (Youth Agency Marketplace) organization
+INSERT INTO [Entity].[Organization]([Id],[Name],[WebsiteURL],[PrimaryContactName],[PrimaryContactEmail],[PrimaryContactPhone],[VATIN],[TaxNumber],[RegistrationNumber]
+           ,[City],[CountryId],[StreetAddress],[Province],[PostalCode],[Tagline],[Biography],[StatusId],[CommentApproval],[DateStatusModified],[LogoId],[DateCreated],[DateModified])
+VALUES(NEWID(),'Yoma (Youth Agency Marketplace)','https://www.yoma.world/','Primary Contact','primarycontact@gmail.com','+27125555555', 'GB123456789', '0123456789', '12345/28/14',
+		'My City',(SELECT [Id] FROM [Lookup].[Country] WHERE CodeAlpha2 = 'ZA'),'My Street Address 1000', 'My Province', '12345-1234','Unlock your future','The yoma platform enables you to build and transform your future by unlocking your hidden potential.',
+		(SELECT [Id] FROM [Entity].[OrganizationStatus] WHERE [Name] = 'Active'),'Approved',GETDATE(), NULL,GETDATE(),GETDATE())
+GO
+
+--organization admins
+INSERT INTO [Entity].[OrganizationUsers]([Id],[OrganizationId],[UserId],[DateCreated])
+SELECT NEWID(), [Id], (SELECT [Id] FROM [Entity].[User] WHERE [Email] = '{org_admin_user}'), GETDATE()
+FROM [Entity].[Organization]
+GO
+
+--ssi tenant creation (pending) for active organizations
+INSERT INTO [SSI].[TenantCreation]([Id],[EntityType],[StatusId],[UserId],[OrganizationId],[TenantId],[ErrorReason],[RetryCount],[DateCreated],[DateModified])
+SELECT NEWID(),'Organization',(SELECT [Id] FROM [SSI].[TenantCreationStatus] WHERE [Name] = 'Pending'),NULL,[Id],NULL,NULL,NULL,GETDATE(),GETDATE()
+FROM [Entity].[Organization]
+WHERE [StatusId] = (SELECT [Id] FROM [Entity].[OrganizationStatus] WHERE [Name] = 'Active')
+GO
+
+--organization provider types
+INSERT INTO [Entity].[OrganizationProviderTypes]([Id],[OrganizationId],[ProviderTypeId],[DateCreated])
+SELECT NEWID(),O.[Id] AS [OrganizationId],PT.[Id] AS [ProviderTypeId],GETDATE() FROM [Entity].[Organization] O CROSS JOIN [Entity].[OrganizationProviderType] PT
+GO
+
+/****opportunity****/
+INSERT INTO [Opportunity].[Opportunity]([Id],
+	[Title],
+	[Description],
+	[TypeId],
+	[OrganizationId],
+	[Summary],
+	[Instructions],
+	[URL],
+	[ZltoReward],
+	[ZltoRewardPool],
+	[ZltoRewardCumulative],
+	[YomaReward],
+	[YomaRewardPool],
+	[YomaRewardCumulative],
+	[VerificationEnabled],
+	[VerificationMethod],
+	[DifficultyId],
+	[CommitmentIntervalId],
+	[CommitmentIntervalCount],
+	[ParticipantLimit],
+	[ParticipantCount],
+	[StatusId],
+	[Keywords],
+	[DateStart],
+	[DateEnd],
+	[CredentialIssuanceEnabled],
+	[SSISchemaName],
+	[DateCreated],
+	[CreatedBy],
+	[DateModified],
+	[ModifiedBy])
+VALUES(NEWID(),
+	N'HEY YOMA USERS 🌟 Your input matters! How does Yoma impact you? Share your thoughts! 🚀(15-30mins)',
+	N'Curious about how Yoma is impacting your social groove? 😃📈 We''re on a quest to understand just that, and we need YOUR voice to uncover the magic! 🎉✨ Take our Relational Wellbeing Impact Survey and let''s dig into the details. 🚀📋 Ready to share your thoughts? Dive in! 👇🤗 i) Click on "Go to Opportuntiy" and you will be redirected to our Google Form. ii) Complete the Survey by answering the questions provided. iii) Don''t forget to Submit Your Responses by clicking on "Submit". Your insights will help us create a Yoma that boosts your connections and happiness! 🚀🤗 iii) Once submitted, take a screenshot and upload the screenshot to earn Zltos',
+	(SELECT [Id] FROM [Opportunity].[OpportunityType] WHERE [Name] = 'Task'),
+	(SELECT [Id] FROM [Entity].[Organization] WHERE [Name] = 'Yoma (Youth Agency Marketplace)'),
+	NULL,
+	NULL,
+	' https://go.yoma.world/impactevaluationsurve',
+	25,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	1,
+	'Manual',
+	(SELECT TOP 1 [Id] FROM [Opportunity].[OpportunityDifficulty] ORDER BY NEWID()),
+	(SELECT TOP 1 [Id] FROM [Lookup].[TimeInterval] ORDER BY NEWID()),
+	(SELECT 1 + ABS(CHECKSUM(NEWID()) % 10)),
+    (SELECT 100 + ABS(CHECKSUM(NEWID()) % 901)),
+	NULL,
+	(SELECT [Id] FROM [Opportunity].[OpportunityStatus] WHERE [Name] = 'Active'),
+	'YOMA,input,your thoughts,impacting,social,groove,magic',
+	'2023-10-01',
+	'2023-12-31',
+	1,
+	'Opportunity|Default',
+	GETDATE(),
+	'{org_admin_user}',
+	GETDATE(),
+	'{org_admin_user}')
+GO
+
+--categories
+INSERT INTO [Opportunity].[OpportunityCategories]([Id],[OpportunityId],[CategoryId],[DateCreated])
+SELECT NEWID(),O.[Id] AS [OpportunityId],OC.[Id] AS [CategoryId],GETDATE() FROM [Opportunity].[Opportunity] O CROSS JOIN [Opportunity].[OpportunityCategory] OC
+GO
+
+--countries
+INSERT INTO [Opportunity].[OpportunityCountries]([Id], [OpportunityId], [CountryID], [DateCreated])
+SELECT NEWID(), O.[Id] AS [OpportunityId], R.CountryID, GETDATE()
+FROM [Opportunity].[Opportunity] O
+CROSS JOIN (
+    SELECT TOP 10 [Id] AS CountryID
+    FROM [Lookup].[Country]
+    ORDER BY NEWID()
+) AS R;
+GO
+
+--languages
+INSERT INTO [Opportunity].[OpportunityLanguages]([Id],[OpportunityId],[LanguageId],[DateCreated])
+SELECT NEWID(), O.[Id] AS [OpportunityId], R.LanguageId, GETDATE()
+FROM [Opportunity].[Opportunity] O
+CROSS JOIN (
+    SELECT TOP 10 [Id] AS LanguageId
+    FROM [Lookup].[Language]
+    ORDER BY NEWID()
+) AS R;
+GO
+
+--skills
+INSERT INTO [Opportunity].[OpportunitySkills]([Id],[OpportunityId],[SkillId],[DateCreated])
+SELECT NEWID(), O.[Id] AS [OpportunityId], R.SkillId, GETDATE()
+FROM [Opportunity].[Opportunity] O
+CROSS JOIN (
+    SELECT TOP 10 [Id] AS SkillId
+    FROM [Lookup].[Skill]
+    ORDER BY NEWID()
+) AS R;
+GO
+
+--verification types
+INSERT INTO [Opportunity].[OpportunityVerificationTypes]([Id],[OpportunityId],[VerificationTypeId],[Description],[DateCreated],[DateModified])
+SELECT NEWID(), O.[Id] AS [OpportunityId], R.[VerificationTypeId], NULL, GETDATE(), GETDATE()
+FROM [Opportunity].[Opportunity] O
+CROSS JOIN (
+    SELECT TOP 10 [Id] AS [VerificationTypeId]
+    FROM [Opportunity].[OpportunityVerificationType]
+    ORDER BY NEWID()
+) AS R
+WHERE O.VerificationEnabled = 1
+GO
