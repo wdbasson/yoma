1498f013463ba5bd3f9587f6ecf67ff57bacae14
Matthew-Baird <148975913+Matthew-Baird@users.noreply.github.com>
2024-04-04 16:12:55 +0200

    Center text on marketplace down banner (#678)

--- src/web/src/components/Status/MarketplaceDown.tsx
+++ src/web/src/components/Status/MarketplaceDown.tsx
@@ -12,7 +12,7 @@ export const MarketplaceDown: React.FC = () => {
       <div className="container flex max-w-7xl flex-col items-center justify-center">
         <div className="flex w-full flex-col items-center justify-center gap-8 rounded-lg bg-white p-4 py-16 shadow-custom">
           <Image src={productsImage} height={100} width={100} alt="Products" />
-          <h2 className="text-2xl font-medium text-gray-dark">
+          <h2 className="text-center text-2xl font-medium text-gray-dark">
             Updating the store, come back soon!
           </h2>
           <p className="text-center text-gray-dark">
